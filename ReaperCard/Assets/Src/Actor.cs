5eb25cf74b671e82091b65527ff7e7d0cf4176ad
Lafolie <longshaw.dale+bitbucket@gmail.com>
2019-04-27 06:35:24 +0100

    Added src, updated settings

---
+++ ReaperCard/Assets/Src/Actor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  Actor
+ *  Serves as the basis for the player and NPC classes, maybe others?
+ * 
+ *  Relevant functionality:
+ * 
+ * 
+ */ 
+
+public class Actor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
