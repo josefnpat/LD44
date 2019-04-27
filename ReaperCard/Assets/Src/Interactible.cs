b2d6bfe32c9ab27d3e9e51f2f7b773e95d96d9d1
Lafolie <longshaw.dale+bitbucket@gmail.com>
2019-04-28 00:36:15 +0100

    Added base interactible

---
+++ ReaperCard/Assets/Src/Interactible.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody))]
+public class Interactible : MonoBehaviour
+{
+    CapsuleCollider capsule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        capsule = GetComponent<CapsuleCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        PlayerComponent player = Other.GetComponent<PlayerComponent>();
+        if(player)
+        {
+            player.TrySetInteractObj(this.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider Other)
+    {
+        PlayerComponent player = Other.GetComponent<PlayerComponent>();
+        if (player)
+        {
+            player.TryClearInteractObj(this.gameObject);
+        }
+    }
+}
---
+++ ReaperCard/Assets/Src/PlayerComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComponent : MonoBehaviour
+{
+    public GameObject InteractObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TrySetInteractObj(GameObject Obj)
+    {
+        InteractObj = Obj;
+    }
+
+    public void TryClearInteractObj(GameObject Obj)
+    {
+        if(InteractObj && InteractObj == Obj)
+        {
+            InteractObj = null;
+        }
+    }
+}
