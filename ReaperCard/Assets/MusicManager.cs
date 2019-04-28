42e24c49d65cc648dad2d6b5d4182774787b85f4
Seppi <josefnpat@gmail.com>
2019-04-28 01:35:59 -0500

    Fixing dialog manager and adding music manager prefab along with some music

---
+++ ReaperCard/Assets/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public string initFile;
+	private AudioSource audioSource;
+
+	void Start() {
+		audioSource = gameObject.GetComponent<AudioSource>();
+		ChangeToInit();
+	}
+
+	public void ChangeToInit() {
+		ChangeTo(initFile);
+	}
+
+	public void ChangeTo(string song) {
+		Debug.Log("Changing music to: " + song);
+		audioSource.Stop();
+		audioSource.clip = Resources.Load("Music/"+song) as AudioClip;
+		//audioSource.clip = Resources.Load<AudioClip>(song);
+		Debug.Log("audioSource.clip:" + audioSource.clip);
+		audioSource.Play();
+	}
+
+}
---
+++ ReaperCard/Assets/MusicManagerEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MusicManager))]
+public class MusicManagerEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		MusicManager myScript = (MusicManager) target;
+
+		if (GUILayout.Button("Change To Init")) {
+			myScript.ChangeToInit();
+		}
+
+	}
+}
