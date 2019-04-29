f0b07425a12c1132ba520652d56942e01ec568fb
Joel Schumacher <joelschum@gmail.com>
2019-04-29 20:44:13 +0200

    Add music fading

--- ReaperCard/Assets/MusicManager.cs
+++ ReaperCard/Assets/MusicManager.cs
@@ -10,8 +10,12 @@ public class MusicManager : MonoBehaviour {
 	}
 	public List<MusicEntry> songs = new List<MusicEntry>();
 	public string initFile;
-	public string currentFile;
+	public float fadeOutTime = 0.8f;
+	public float fadeInTime = 0.8f;
+
 	private AudioSource audioSource;
+	private MusicEntry currentSong;
+	private MusicEntry fadeIntoSong;
 
 	void Start() {
 		audioSource = gameObject.GetComponent<AudioSource>();
@@ -23,19 +27,52 @@ public class MusicManager : MonoBehaviour {
 	}
 
 	public void ChangeTo(string songName) {
-		if (songName == currentFile) {
+		songName = songName.Trim();
+		var song = songs.Find(item => item.name == songName);
+
+		if (song != null && song == currentSong) {
+			Debug.Log("Play current song -> abort!");
 			return;
 		}
-		currentFile = songName;
-		songName = songName.Trim();
+
+		if(songName.Length == 0) {
+			Debug.Log("Stopping music.");
+			audioSource.Stop();
+			currentSong = null;
+			return;
+		}
+
+		Debug.Assert(song != null, "Unknown song with name " + songName);
 		Debug.Log("Changing music to: " + songName);
-		audioSource.Stop();
-		if(songName.Length > 0) {
-			var song = songs.Find(item => item.name == songName);
-			Debug.Assert(song != null, "Unknown song with name " + songName);
+		if(currentSong == null) {
+			Debug.Log("play new song");
 			audioSource.clip = song.song;
-			Debug.Log("audioSource.clip:" + audioSource.clip);
 			audioSource.Play();
+			currentSong = song;
+		} else {
+			Debug.Log("Set fadeinto");
+			fadeIntoSong = song;
+		}
+	}
+
+	void Update() {
+		if(currentSong != null) {
+			if(fadeIntoSong == null) {
+				if(currentSong != null) {
+					var volume = audioSource.volume + Time.deltaTime / fadeInTime;
+					audioSource.volume = Mathf.Min(1f, volume);
+				}
+			} else {
+				var volume = audioSource.volume - Time.deltaTime / fadeOutTime;
+				audioSource.volume = Mathf.Max(0f, volume);
+				if(volume < 0) {
+					audioSource.Stop();
+					audioSource.clip = fadeIntoSong.song;
+					audioSource.Play();
+					currentSong = fadeIntoSong;
+					fadeIntoSong = null;
+				}
+			}
 		}
 	}
 }
