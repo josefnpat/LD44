f4f5da1b4da68068455b8bffdd0d1d00ad10f480
Joel Schumacher <joelschum@gmail.com>
2019-04-30 00:30:51 +0200

    Add readme, move items, fix float, be tired, cry

--- ReaperCard/Assets/Float.cs
+++ ReaperCard/Assets/Float.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class Float : MonoBehaviour
 {
     [Header("Floating Settings")]
-    public float floatOffsetY = 1.53f;
     public float floatSpeed = 8f;
     public float floatRadius = 1.5f;
     private float floatTimeOffset = 0f;
+    private float startY;
 
     void Start() {
       floatTimeOffset = Random.Range(0, 100);
+      startY = transform.localPosition.y;
     }
     void Update()
     {
       Vector3 offset = transform.localPosition;
-      offset.y = Mathf.Sin(Time.time / floatSpeed + floatTimeOffset) * floatRadius + floatOffsetY;
+      offset.y = startY + Mathf.Sin(Time.time / floatSpeed + floatTimeOffset) * floatRadius;
       //print(y);
       transform.localPosition = offset;
     }
