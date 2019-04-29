75445c98a30e210999f07b57da9abaf4b5b94af8
Seppi <josefnpat@gmail.com>
2019-04-28 19:03:50 -0500

    Quit on escape

---
+++ ReaperCard/Assets/QuitOnEscape.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnEscape : MonoBehaviour {
+
+	void Update() {
+		if (transform.position.y < -200){
+			Debug.Log("Game Over.");
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+			 Application.OpenURL(webplayerQuitURL);
+#else
+			 Application.Quit();
+#endif
+		}
+	}
+}
